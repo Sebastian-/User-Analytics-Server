2d911ead2407dc5c39896cad32393cab3a12a670
Sebastian- <hmurgu@interchange.ubc.ca>
2018-08-20 07:29:28 -0700

    Added brute force methods for computing loyal user statistics

--- brute_force.py
+++ brute_force.py
@@ -8,6 +8,13 @@ Users using OS 1: 1249619
 Users using OS 0 and 6: 1477249
 Users using device 3: 767244
 Users using device 0 and 5: 1285700
+Users using OS 1 and device 1: 6143198
+Number of loyal users is 563740
+Loyal users using OS 1: 81413
+Loyal users using OS 0 and 6: 140467
+Loyal users using device 3: 79508
+Loyal users using device 0 and 5: 24561
+Loyal users using OS 1 and device 1: 414706
 
 """
 
@@ -21,6 +28,18 @@ def main():
     print("Users using OS 0 and 6: " + str(users_with_os(0, 6)))
     print("Users using device 3: " + str(users_with_device(3)))
     print("Users using device 0 and 5: " + str(users_with_device(0, 5)))
+    print("Users using OS 1 and device 1: " + str(users_combined(1, 1)))
+    print("Number of loyal users is " + str(num_of_loyal_users()))
+    print("Loyal users using OS 1: " + str(loyal_with_os(1)))
+    print("Loyal users using OS 0 and 6: " + str(loyal_with_os(0, 6)))
+    print("Loyal users using device 3: " + str(loyal_with_device(3)))
+    print("Loyal users using device 0 and 5: " + str(loyal_with_device(0, 5)))
+    print("Loyal users using OS 1 and device 1: " + str(loyal_combined(1, 1)))
+
+
+def num_of_unique_users():
+    """Returns the number of unique users"""
+    return len(users)
 
 
 def users_with_os(*args):
@@ -42,6 +61,70 @@ def users_with_device(*args):
             if device in userInfo['device']:
                 count += 1
                 break;
+    return count
+
+
+def users_combined(os, device):
+    """Returns the number of users using both os and device"""
+    count = 0
+    for userInfo in users.values():
+        if os in userInfo['os']:
+            count += 1
+            continue
+        if device in userInfo['device']:
+            count += 1
+            continue
+    return count
+
+
+
+def num_of_loyal_users():
+    """Returns the number of loyal users"""
+    count = 0
+    for userInfo in users.values():
+        if userInfo['visits'] >= 10:
+            count += 1
+    return count
+
+
+def loyal_with_os(*args):
+    """Returns the number of loyal users using an OS in args"""
+    count = 0
+    for userInfo in users.values():
+        if userInfo['visits'] < 10:
+            continue
+        for os in args:
+            if os in userInfo['os']:
+                count += 1
+                break;
+    return count
+
+
+def loyal_with_device(*args):
+    """Returns the number of loyal users using a device in args"""
+    count = 0
+    for userInfo in users.values():
+        if userInfo['visits'] < 10:
+            continue
+        for device in args:
+            if device in userInfo['device']:
+                count += 1
+                break;
+    return count
+
+
+def loyal_combined(os, device):
+    """Returns the number of loyal users using both os and device"""
+    count = 0
+    for userInfo in users.values():
+        if userInfo['visits'] < 10:
+            continue
+        if os in userInfo['os']:
+            count += 1
+            continue
+        if device in userInfo['device']:
+            count += 1
+            continue
     return count
 
 
@@ -87,12 +170,6 @@ def has_consistent_user_environment():
             users[userData[1]] = {'os': userData[2], 'device': userData[3]}
 
     return True;
-
-
-def num_of_unique_users():
-    """Returns the number of unique users"""
-    load_data()
-    return len(users)
 
 
 if __name__ == '__main__':
