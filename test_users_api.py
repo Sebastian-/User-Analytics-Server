d60651a57ab4cffd2563a143b25f5e8350ae4b6b
Sebastian- <hmurgu@interchange.ubc.ca>
2018-08-19 20:21:25 -0700

    Added test for route /unique-users?os=1

--- test_users_api.py
+++ test_users_api.py
@@ -11,6 +11,7 @@ class TestUsersAPI(TestCase):
     def create_app(self):
         return app
 
+
     def test_ping(self):
         """Ensure the /ping route behaves correctly."""
         response = self.client.get('/ping')
@@ -19,6 +20,7 @@ class TestUsersAPI(TestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+
     def test_unique_users(self):
         """Ensure /unique-users response matches the brute forced value"""
         response = self.client.get('/unique-users')
@@ -26,6 +28,13 @@ class TestUsersAPI(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(8550022, data['count'])
 
+
+    def test_unique_users_single_os(self):
+        """Ensure /unique-users?os=1 response matches the brute forced value"""
+        response = self.client.get('/unique-users?os=1')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(1249619, data['count'])
 
 
 if __name__ == '__main__':
